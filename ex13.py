a62d6da6ae6f1575d89302138521e27dec7b588d
jerry <orzjerry@gmail.com>
2014-12-30 09:05:50 +0800

    12-30 ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "the script is called:", script
+print "Your first variable is:", first
+print "Your second varible is:", second
+print "Your third cariable is;", third
